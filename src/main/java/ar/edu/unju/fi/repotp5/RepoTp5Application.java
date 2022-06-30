e51be0ba57437a2d026d510d805650c218a31e0d
Sebas <sebas.654@gmail.com>
2022-06-30 00:37:14 -0300

    Incio tp05

---
+++ src/main/java/ar/edu/unju/fi/repotp5/RepoTp5Application.java
@@ -0,0 +1,13 @@
+package ar.edu.unju.fi.repotp5;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RepoTp5Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RepoTp5Application.class, args);
+	}
+
+}
---
+++ src/test/java/ar/edu/unju/fi/repotp5/RepoTp5ApplicationTests.java
@@ -0,0 +1,13 @@
+package ar.edu.unju.fi.repotp5;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RepoTp5ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
